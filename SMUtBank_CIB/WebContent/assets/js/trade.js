bb6f002a6d626c90e62dd5faeb2b051ff820cc2e
Yashraj Jalota <jalota16@gmail.com>
2015-07-22 16:13:55 +0800

    LC Enquiry Sort

--- SMUtBank_CIB/WebContent/assets/js/trade.js
+++ SMUtBank_CIB/WebContent/assets/js/trade.js
@@ -18,7 +18,9 @@ trade.init = function() {
 		LCEnquiry: stencil.define("LCEnquiry","#importerFunctions"),
 		LCList : stencil.define("LCList","#LCList"),
 		LClistError: stencil.define("LClistError","none"),
-		LCListHeader: stencil.define("LCListHeader","#LCListHeader")
+		LCListHeader: stencil.define("LCListHeader","#LCListHeader"),
+		searchLCList: stencil.define("searchLCList","#searchLCList")
+		
 	};
 };
 
@@ -161,24 +163,29 @@ trade.getLCList = function(){
 		var message = "";
 		LC.LCList  = response;
         message = response.esbStatus;
-        document.getElementById("messageforenquiry").innerHTML = message;
+        console.log(message);
         console.log(response);
         console.log(extras);
         var LCData = [];
         if(response["ns:ref_num.length"] > 0) {
             for(var i = 1; i <= response["ns:ref_num.length"]; i++) {
-            	
+            	//var searchterm = document.getElementById("LCSearch").value;
+            	//if(response["ns:ref_num" + i] == searchterm || response["ns:status" + i] == searchterm  ){
                 LCData.push({
                     LC_ref_num: response["ns:ref_num" + i],
                 	LC_currency: response["ns:currency" + i],
                 	LC_status: response["ns:status" + i],
                 	LC_amount:  response["ns:amount" + i],
-                	LC_ship_date: response["ns:ship_date" + i]
+                	LC_ship_date: response["ns:ship_date" + i],
+                	LC_creation: response["ns:creation_datetime" + i]
                     
                 });
+            //}
             }
+            trade.stencils.searchLCList.render({});
             trade.stencils.LCListHeader.render({});
             trade.stencils.LCList.render(LCData);
+            
             //document.getElementById("doGiroDelete").style.display = '';
         }else {
         	
@@ -199,7 +206,136 @@ trade.getLCList = function(){
 		+ '", "end_datetime":"'
 		+  endDate + '"}';
 	
-	network.doESB(populater, "Trade_LCList_ReadResponse", '["ns:ref_num","ns:status","ns:ship_date","ns:currency","ns:amount"]', payload, null, true);
+	network.doESB(populater, "Trade_LCList_ReadResponse", '["ns:ref_num","ns:status","ns:ship_date","ns:currency","ns:amount","ns:creation_datetime"]', payload, null, true);
+	
+};
+
+trade.getLCListSorted = function(){
+	
+	var populater = function(response, extras) {
+		console.log(response.esbStatus);
+		var message = "";
+		LC.LCList  = response;
+        message = response.esbStatus;
+        console.log(message);
+        console.log(response);
+        console.log(extras);
+        var LCData = [];
+        if(response["ns:ref_num.length"] > 0) {
+            for(var i = 1; i <= response["ns:ref_num.length"]; i++) {
+            	//var searchterm = document.getElementById("LCSearch").value;
+            	//if(response["ns:ref_num" + i] == searchterm || response["ns:status" + i] == searchterm  ){
+                LCData.push({
+                    LC_ref_num: response["ns:ref_num" + i],
+                	LC_currency: response["ns:currency" + i],
+                	LC_status: response["ns:status" + i],
+                	LC_amount:  response["ns:amount" + i],
+                	LC_ship_date: response["ns:ship_date" + i],
+                	LC_creation: response["ns:creation_datetime" + i]
+                    
+                });
+                
+            //}
+            }
+            function compare(a,b) {
+            	  if (a.LC_status < b.LC_status)
+            	    return -1;
+            	  if (a.LC_status > b.LC_status)
+            	    return 1;
+            	  return 0;
+            	}
+
+            LCData.sort(compare);
+            
+            trade.stencils.searchLCList.render({});
+            trade.stencils.LCListHeader.render({});
+            trade.stencils.LCList.render(LCData);
+            
+            //document.getElementById("doGiroDelete").style.display = '';
+        }else {
+        	
+            $("#LCList").empty().append(trade.stencils.LClistError.render({errorMessage: "You have no LCs"}, "fragment"));
+            //document.getElementById("doGiroDelete").style.display = 'none';
+        }
+
+       
+	};
+	var customerID = document.getElementById("customerID").value;
+	var startDate = document.getElementById("startDate").value;
+	var endDate = document.getElementById("endDate").value;
+	console.log(customerID);
+	var payload = '{"ServiceDomain":"Trade","OperationName":"Trade_LCList_Read", "customer_ID":"'
+		+ customerID
+		+ '", "start_datetime":"'
+		+ startDate
+		+ '", "end_datetime":"'
+		+  endDate + '"}';
+	
+	network.doESB(populater, "Trade_LCList_ReadResponse", '["ns:ref_num","ns:status","ns:ship_date","ns:currency","ns:amount","ns:creation_datetime"]', payload, null, true);
+	
+};
+trade.getLCListSortedByRef = function(){
+	
+	var populater = function(response, extras) {
+		console.log(response.esbStatus);
+		var message = "";
+		LC.LCList  = response;
+        message = response.esbStatus;
+        console.log(message);
+        console.log(response);
+        console.log(extras);
+        var LCData = [];
+        if(response["ns:ref_num.length"] > 0) {
+            for(var i = 1; i <= response["ns:ref_num.length"]; i++) {
+            	//var searchterm = document.getElementById("LCSearch").value;
+            	//if(response["ns:ref_num" + i] == searchterm || response["ns:status" + i] == searchterm  ){
+                LCData.push({
+                    LC_ref_num: response["ns:ref_num" + i],
+                	LC_currency: response["ns:currency" + i],
+                	LC_status: response["ns:status" + i],
+                	LC_amount:  response["ns:amount" + i],
+                	LC_ship_date: response["ns:ship_date" + i],
+                	LC_creation: response["ns:creation_datetime" + i]
+                    
+                });
+                
+            //}
+            }
+            function compare(a,b) {
+            	  if (a.LC_ref_num < b.LC_ref_num)
+            	    return -1;
+            	  if (a.LC_ref_num > b.LC_ref_num)
+            	    return 1;
+            	  return 0;
+            	}
+
+            LCData.sort(compare);
+            
+            trade.stencils.searchLCList.render({});
+            trade.stencils.LCListHeader.render({});
+            trade.stencils.LCList.render(LCData);
+            
+            //document.getElementById("doGiroDelete").style.display = '';
+        }else {
+        	
+            $("#LCList").empty().append(trade.stencils.LClistError.render({errorMessage: "You have no LCs"}, "fragment"));
+            //document.getElementById("doGiroDelete").style.display = 'none';
+        }
+
+       
+	};
+	var customerID = document.getElementById("customerID").value;
+	var startDate = document.getElementById("startDate").value;
+	var endDate = document.getElementById("endDate").value;
+	console.log(customerID);
+	var payload = '{"ServiceDomain":"Trade","OperationName":"Trade_LCList_Read", "customer_ID":"'
+		+ customerID
+		+ '", "start_datetime":"'
+		+ startDate
+		+ '", "end_datetime":"'
+		+  endDate + '"}';
+	
+	network.doESB(populater, "Trade_LCList_ReadResponse", '["ns:ref_num","ns:status","ns:ship_date","ns:currency","ns:amount","ns:creation_datetime"]', payload, null, true);
 	
 };
 
